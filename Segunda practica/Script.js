c590fb86ae2d4442031a7fd1102288686c5b8fed
Nicolis15 <Nicolistrabajo@gmail.com>
2024-09-08 18:52:58 -0500

    Trabajo para la clase

--- Segunda practica/Script.js
+++ Segunda practica/Script.js
@@ -1,12 +1,21 @@
-alert("Se cargo el js")
+//alert("Se cargo el js")
 const textinput = document.querySelector("input");
 const btngenerar = document.querySelector(".btn_generar");
+const imagen = document.querySelector("img")
+
 
 btngenerar.addEventListener("click", event =>{
     event.preventDefault();
+    if(imagen.style.display === 'none'){
+        imagen.style.display = 'block';
+    }
+    else{
+        imagen.style.display = 'none'
+    }
     const texto = textinput.value;
     console.log(texto)
-    if(!texto){
+    /*if(!texto){
         alert("No se ingreso ningun texto para generar el codigo QR");
-    }
+    }*/
 });
+
